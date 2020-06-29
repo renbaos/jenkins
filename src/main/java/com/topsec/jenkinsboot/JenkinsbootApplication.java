49efe895581f570b1642cc41e8eeaa11d4fdb387
ren_baoshen <ren_baoshen@topsec.com.cn>
2020-06-29 13:36:25 +0800

    tset

---
+++ src/main/java/com/topsec/jenkinsboot/JenkinsbootApplication.java
@@ -0,0 +1,13 @@
+package com.topsec.jenkinsboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsbootApplication.class, args);
+	}
+
+}
