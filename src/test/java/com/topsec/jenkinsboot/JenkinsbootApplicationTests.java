836ee7e73263553dacd6e775120fd60ac22f0bb2
ren_baoshen <ren_baoshen@topsec.com.cn>
2020-06-29 14:03:58 +0800

    add a test controller

---
+++ src/test/java/com/topsec/jenkinsboot/JenkinsbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topsec.jenkinsboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
